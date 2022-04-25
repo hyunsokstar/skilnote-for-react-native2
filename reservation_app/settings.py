b77734e2b72fd401ff9d98b7aa98b0a85ca6942e
hyunsok <terecal@daum.net>
2022-04-25 15:33:32 +0900

    update for host

--- reservation_app/settings.py
+++ reservation_app/settings.py
@@ -13,7 +13,7 @@ DEBUG = True
 # ALLOWED_HOSTS = []
 # ALLOWED_HOSTS = ['158.247.227.197', '127.0.0.1',
 #                  'www.skilnote-for-jpa.co.kr', 'skilnote-for-innotree.co.kr']
-ALLOWED_HOSTS = ['141.164.51.42','127.0.0.1', 'www.skilnote-for-react-native.shop', 'skilnote-for-react-native.shop', '141.164.51.42.vultrusercontent.com']
+ALLOWED_HOSTS = ['158.247.236.160','127.0.0.1', 'www.skilnote-for-react-native.shop', 'skilnote-for-react-native.shop', '141.164.51.42']
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Application definition
